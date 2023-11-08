155ba2fc989c1f4bd5a673662841dfe5e051f95f
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-11-08 18:40:28 +1300

    add `/link` command

--- src/commands/meta/commandMap.ts
+++ src/commands/meta/commandMap.ts
@@ -1,12 +1,18 @@
 import { Command } from '../../types/Command';
 import { helpCommand } from '../help';
+import { linkCommand } from '../link';
 import { listCommand } from '../list';
 import { statsCommand } from '../stats';
 import { statusCommand } from '../status';
 import { topCommand } from '../top';
 
 export const commandMap = new Map<string, Command>(
-    [helpCommand, listCommand, statsCommand, statusCommand, topCommand].map(
-        (e) => [e.name, e],
-    ),
+    [
+        helpCommand,
+        listCommand,
+        statsCommand,
+        statusCommand,
+        topCommand,
+        linkCommand,
+    ].map((e) => [e.name, e]),
 );
