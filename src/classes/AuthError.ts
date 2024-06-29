ff4e6c64b4b73e9be36b5d34c1ba9cee6ea2cb0b
NachoToast <32235595+NachoToast@users.noreply.github.com>
2024-06-29 23:23:09 +1200

    update error class descriptions

--- src/classes/AuthError.ts
+++ src/classes/AuthError.ts
@@ -3,7 +3,8 @@ import { SiteError } from './SiteError.js';
 /**
  * Error thrown when a user's site token (JWT) is missing or invalid.
  *
- * Has status code 401 (Unauthorized), since the user has failed authentication.
+ * Has status code 401 (Unauthorized), since the user has failed
+ * authentication.
  */
 export class AuthError extends SiteError<null> {
     public readonly statusCode = 401;
--- src/classes/NotFoundError.ts
+++ src/classes/NotFoundError.ts
@@ -1,9 +1,10 @@
 import { SiteError } from './SiteError.js';
 
 /**
- * Error thrown when a server or user does not exist in the database.
+ * Error thrown when an item does not exist in the database.
  *
- * Has status code 404 (Not Found), since the user has requested a resource that does not exist.
+ * Has status code 404 (Not Found), since the user has requested a resource
+ * that does not exist.
  */
 export class NotFoundError extends SiteError<null> {
     public readonly statusCode = 404;
