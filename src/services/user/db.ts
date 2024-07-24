ef6fe2484249e3bf6cc4924e20eb1a297dda1f1b
NachoToast <32235595+NachoToast@users.noreply.github.com>
2024-07-24 16:37:12 +1200

    user db initialisation consistency

--- src/services/user/db.ts
+++ src/services/user/db.ts
@@ -7,9 +7,5 @@ export type UserDocument = WithId<Omit<DeepRequired<User>, 'id'>>;
 let userDb: Collection<UserDocument> | null = null;
 
 export function getUserDb(): Collection<UserDocument> {
-    if (userDb === null) {
-        userDb = AppGlobals.db.collection('users');
-    }
-
-    return userDb;
+    return (userDb ??= AppGlobals.db.collection('users'));
 }
--- src/services/user/initialise.ts
+++ src/services/user/initialise.ts
@@ -1,12 +1,9 @@
 import { getUserDb } from './db.js';
 
 export async function initialise(): Promise<void> {
-    await getUserDb().createIndexes(
-        [
-            { key: { 'steam.id': 1 } },
-            { key: { 'discord.id': 1 }, unique: true },
-            { key: { 'steam.username': 'text', 'discord.username': 'text' } },
-        ],
-        {},
-    );
+    await getUserDb().createIndexes([
+        { key: { 'steam.id': 1 } },
+        { key: { 'discord.id': 1 }, unique: true },
+        { key: { 'steam.username': 'text', 'discord.username': 'text' } },
+    ]);
 }
