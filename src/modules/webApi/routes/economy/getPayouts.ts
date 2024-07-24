4c8c0921bce634d0bc975ad945a6bb60b9097544
NachoToast <32235595+NachoToast@users.noreply.github.com>
2024-07-24 16:42:22 +1200

    misc syntax update

--- src/modules/webApi/routes/economy/getPayouts.ts
+++ src/modules/webApi/routes/economy/getPayouts.ts
@@ -5,30 +5,28 @@ import { EconomyService } from '../../../../services/index.js';
 import { AuthScope } from '../../types/auth/AuthScope.js';
 import { EndpointProvider } from '../../types/express/EndpointProvider.js';
 
-interface PayoutsQuery {
+interface Query {
     steamIds: SteamId64[];
 }
 
 const tokens = new Set<string>();
 
-export const getPayouts: EndpointProvider<void, Payout[], void, PayoutsQuery> =
-    {
-        auth: AuthScope.Plugin,
-        onStart() {
-            for (const token of AppGlobals.config.modules.webApi
-                .economyBotTokens) {
-                tokens.add(token);
-            }
-        },
-        isTokenValid(token) {
-            return tokens.has(token);
-        },
-        async handleRequest({ req, res }) {
-            console.log(req.query);
-            const data = await EconomyService.getPayoutsBySteamId(
-                req.query.steamIds,
-            );
+export const getPayouts: EndpointProvider<void, Payout[], void, Query> = {
+    auth: AuthScope.Plugin,
+    onStart() {
+        for (const token of AppGlobals.config.modules.webApi.economyBotTokens) {
+            tokens.add(token);
+        }
+    },
+    isTokenValid(token) {
+        return tokens.has(token);
+    },
+    async handleRequest({ req, res }) {
+        console.log(req.query);
+        const data = await EconomyService.getPayoutsBySteamId(
+            req.query.steamIds,
+        );
 
-            res.status(200).json(data.map((payout) => convertId(payout)));
-        },
-    };
+        res.status(200).json(data.map((payout) => convertId(payout)));
+    },
+};
