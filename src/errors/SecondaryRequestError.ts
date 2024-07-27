e8bb090cc4fd987b98fa17bde1b88d8e6a35cb27
NachoToast <32235595+NachoToast@users.noreply.github.com>
2024-07-27 20:52:09 +1200

    502 -> 501 responses

--- src/errors/SecondaryRequestError.ts
+++ src/errors/SecondaryRequestError.ts
@@ -3,11 +3,11 @@ import { SiteError } from './SiteError.js';
 /**
  * Error thrown when an API call made by the server to another server fails.
  *
- * Has status code 502 (Bad Gateway), since the server is acting as a gateway
+ * Has status code 501 (Bad Gateway), since the server is acting as a gateway
  * or proxy and received an invalid response from the upstream server.
  */
 export class SecondaryRequestError extends SiteError<string | null> {
-    public readonly statusCode = 502;
+    public readonly statusCode = 501;
 
     public constructor(
         title: Capitalize<string>,
