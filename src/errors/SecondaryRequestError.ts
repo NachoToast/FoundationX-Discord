8f41f841d88621a4e2c7c5821743a4265b1a7dda
NachoToast <32235595+NachoToast@users.noreply.github.com>
2024-07-27 20:57:48 +1200

    502 -> 501 justification

--- src/errors/SecondaryRequestError.ts
+++ src/errors/SecondaryRequestError.ts
@@ -3,8 +3,9 @@ import { SiteError } from './SiteError.js';
 /**
  * Error thrown when an API call made by the server to another server fails.
  *
- * Has status code 501 (Bad Gateway), since the server is acting as a gateway
- * or proxy and received an invalid response from the upstream server.
+ * Although this **should** have a status code of 501 (Bad Gateway), Cloudflare
+ * likes to overwrite 502 errors with it's own error page, so 501 is used
+ * instead.
  */
 export class SecondaryRequestError extends SiteError<string | null> {
     public readonly statusCode = 501;
